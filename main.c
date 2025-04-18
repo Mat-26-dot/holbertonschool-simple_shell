de2f78bfc505fd4a5851c339ce4bf3f7088d7f9f
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-04-18 01:42:55 +0000

    main function start NO COMMENT NOT TESTED

---
+++ main.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+int main(void)
+{
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t rd = 0;
+	char *args[MAX_ARGS];
+	char *cmd_path = "";
+	pid_t pid;
+	int status;
+
+	while (1)
+	{
+		prompt();
+		rd = getline(&line, &len, stdin);
+		if (rd == -1)
+		{
+			if (feof(stdin))
+				break;
+			perror("getline");
+			continue;
+		}
+
+		parse_line(line, args);
+
+		if (args[0] == NULL)
+			continue;
+		if (handle_builtin(args))
+			continue;
+
+		cmd_path = find_in_path(args[0]);
+		if (!cmd_path)
+		{
+			fprintf(stderr, "%s: command not found\n", args[0]);
+			continue;
+		}
+		pid = fork();
+
+		if (pid == 0)
+		{
+			execve(cmd_path, args, environ);
+			perror("execve");
+			exit(EXIT_FAILURE);
+		}
+		else if (pid < 0)
+			perror("fork");
+		else
+			waitpid(pid, &status, 0);
+	}
+	free(line);
+	return (0);
+}
--- main.h
+++ main.h
@@ -1,7 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
+#define MAX_ARGS 64
+#define DELIM " \t\r\n\a"
+
+extern char **environ;
+
+void prompt(void);
+void parse_line(char *line, char **args);
+int command(char **args);
+char *find_in_path(char *command);
 
 
 

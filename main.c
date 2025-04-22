3e445ce4962f6bfeb58674d7550e4affb2edd113
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-04-22 11:41:53 +0000

    adjustments to main.c

--- main.c
+++ main.c
@@ -37,7 +37,7 @@ int main(void)
 		if (!cmd_path)
 		{
 			fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
-			status = 2;
+			status = 127;
 			continue;
 		}
 		pid = fork();
@@ -55,6 +55,8 @@ int main(void)
 		else
 		{
 			waitpid(pid, &status, 0);
+			if (WIFEXITED(status))
+				status = WEXITSTATUS(status);
 			free(cmd_path);
 
 		}

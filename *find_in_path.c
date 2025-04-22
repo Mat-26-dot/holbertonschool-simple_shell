6991fc7f79850742db7b0a3e90ce30b5e8154f81
JonathanCharalambous <jonathancharalambous30@gmail.com>
2025-04-22 06:17:38 +0000

    adjustments main.c

--- *find_in_path.c
+++ *find_in_path.c
@@ -3,18 +3,27 @@
 char *find_in_path(char *cmd)
 {
 	char *path = _getenv("PATH");
-	char *token = strtok(strdup(path), ":");
+	char *path_copy = strdup(path);
+	char *token = strtok(path_copy, ":");
 	static char full_path[1024];
 
 	if (strchr(cmd, '/'))
+	{
+		free(path_copy);
 		return cmd;
+	}
 
 	while (token != NULL)
 	{
 		snprintf(full_path, sizeof(full_path), "%s/%s", token, cmd);
 		if (access(full_path, X_OK) == 0)
+		{
+			free(path_copy);
 			return (full_path);
+		}
 		token = strtok(NULL, ":");
 	}
+
+	free(path_copy);
 	return (NULL);
 }

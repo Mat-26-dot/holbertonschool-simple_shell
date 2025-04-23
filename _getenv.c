a5ae7c11c69f386641c053f5950de7bbc0779321
mat <md@outlook.com>
2025-04-23 06:50:09 +0000

    Added comments *_getenv.c

--- _getenv.c
+++ _getenv.c
@@ -5,11 +5,12 @@ char *_getenv(const char *name)
 	int i = 0;
 	size_t len = strlen(name);
 
-	for (i = 0; environ[i]; i++)
+	for (i = 0; environ[i]; i++)	/* Loop through the environment variables */
 	{
-		if (strncmp(environ[i], name, len) == 0 && environ[i][len] == '=')
-			return (environ[i] + len + 1);
-	}
-
+		if (strncmp(environ[i], name, len) == 0 && environ[i][len] == '=')	/* See if the start of this env variable matches 'name' */
+											/* strncmp just checks the first few letters (length of 'name') */
+		 		return (environ[i] + len + 1);				/* Also check if the next char is '=' so we only get exact matches */
+	}		/* Got a match, so just return where the value starts */
 	return (NULL);
+	/* Didn't find variable, so return NULL */
 }

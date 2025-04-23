6f5a065e521aaa1c5472f9a73cf428d41d1a16e4
mat <md@outlook.com>
2025-04-23 06:09:21 +0000

    Added comments *_getline.c

--- _getline.c
+++ _getline.c
@@ -2,16 +2,16 @@
 
 ssize_t _getline(char **lineptr, size_t *n)
 {
-	size_t bufsize = 1024;
-	ssize_t i = 0;
-	char c = '\0';
+	size_t bufsize = 1024;	/* Start with a buffer size of 1024 */
+	ssize_t i = 0;		/* Keeps track of how many chars we've read */
+	char c = '\0';		/* This will hold each char we read */
 	char *buffer;
 
-	if (*lineptr == NULL || *n == 0)
+	if (*lineptr == NULL || *n == 0)	/* If the buffer doesn't exist yet, allocate it */
 	{
 		*lineptr = malloc(bufsize);
 		if (!*lineptr)
-			return -1;
+			return -1;	/* Return -1 if malloc fails */
 		*n = bufsize;
 	}
 
@@ -19,25 +19,26 @@ ssize_t _getline(char **lineptr, size_t *n)
 
 	while (1)
 	{
-		ssize_t r = read(STDIN_FILENO, &c, 1);
-		if (r == -1 || (r == 0 && i == 0))
+		ssize_t r = read(STDIN_FILENO, &c, 1);	/* Read one character from stdin */
+		if (r == -1 || (r == 0 && i == 0))	/* If read fails, or nothing to read and nothing read yet, return -1 */
 			return -1;
-		if (r == 0)
+		if (r == 0)			/* If we hit end-of-file after reading somrthing, break out of loop */
 			break;
-		if (i >= (ssize_t)(*n - 1))
+		if (i >= (ssize_t)(*n - 1))	/* If buffer is full, make it bigger */
 		{
-			bufsize *= 2;
+			bufsize *= 2;		/* Double the buffer size */
 			buffer = realloc(buffer, bufsize);
 			if (!buffer)
-				return -1;
+				return -1; 	/* realloc failed */
 			*lineptr = buffer;
 			*n = bufsize;
 		}
-		buffer[i++] = c;
-		if (c == '\n')
+		buffer[i++] = c;	/* Add the character to the buffer */
+		if (c == '\n')		/* If we hit a newline, stop reading */
 			break;
 	}
-	buffer[i] = '\0';
-	return i;
+	buffer[i] = '\0';	/* Null-terminate the string */	
+        }
+	return i;	/* Return how many characters we read */
 }
 
